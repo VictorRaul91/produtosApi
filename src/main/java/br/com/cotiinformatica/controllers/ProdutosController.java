6e1545e4a28deab5d2ee734774b6716b15f82e80
joadson7 <joadsontorres6@gmail.com>
2025-02-18 13:07:01 -0300

    18-02-2025

--- src/main/java/br/com/cotiinformatica/controllers/ProdutosController.java
+++ src/main/java/br/com/cotiinformatica/controllers/ProdutosController.java
@@ -1,47 +1,84 @@
 package br.com.cotiinformatica.controllers;
 
-import java.util.List;
+import java.util.UUID;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.cotiinformatica.dtos.ApiResponseDto;
+import br.com.cotiinformatica.dtos.ProdutoRequestDto;
 import br.com.cotiinformatica.entities.Produto;
+import br.com.cotiinformatica.repositories.CategoriaRepository;
+import br.com.cotiinformatica.repositories.ProdutoRepository;
 import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/produtos")
-
 public class ProdutosController {
 
+	private ModelMapper mapper = new ModelMapper();
+	
 	@Operation(summary = "Serviço para cadastro de produto.")
 	@PostMapping("cadastrar")
-	public void cadastrar() {
+	public ResponseEntity<ApiResponseDto> cadastrar(@RequestBody @Valid ProdutoRequestDto request) {
 
-		// todo
+		var response = new ApiResponseDto();
+		
+		
+		try {
+			
+			//verificar se a categoria não existe no banco de dados
+			var categoriaRepository = new CategoriaRepository();
+			
+			if(!categoriaRepository.exists(request.getCategoriaId())) {
+				response.setMessage("Categoria não encontrada.Verifique categoria informada.");
+				response.setStatusCode(400);  //bad request
+				
+			}else {
+						
+			var produto = mapper.map(request, Produto.class); //capturar os dados do produto
+			produto.setId(UUID.randomUUID()); //gerando um ID para gravar o produto
+			
+			//gravar o produto no banco de dados
+			var produtoRepository = new ProdutoRepository();
+			produtoRepository.inserir(produto, request.getCategoriaId());
+			
+			response.setMessage("Produto cadastrado com sucesso.");
+			response.setId(produto.getId());
+			response.setStatusCode(201);
+			}
+		}
+		catch(Exception e) {
+			response.setMessage(e.getMessage());
+			response.setStatusCode(500);
+		}	
+		
+		return ResponseEntity.status(response.getStatusCode()).body(response);
 	}
-	
-	@Operation(summary = "Servico para atualizar  produto.")
+
+	@Operation(summary = "Serviço para atualização de dados de produto.")
 	@PutMapping("atualizar")
 	public void atualizar() {
-		
+		// TODO Implementar a atualização
 	}
-	
-	@Operation(summary = "Servico para excluir produto.")
+
+	@Operation(summary = "Serviço para exclusão de um produto.")
 	@DeleteMapping("excluir")
 	public void excluir() {
-		
+		// TODO Implementar a exclusão
 	}
-	
-	@Operation(summary = "Servico para consulta de todos os produto.")
+
+	@Operation(summary = "Serviço para consulta de todos os produtos.")
 	@GetMapping("consultar")
 	public void consultar() {
-		
+		// TODO Implementar a exclusão
 	}
 }
